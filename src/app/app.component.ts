02977853dc88728eb36283bd60b0f96ded64a954
Sunny Tsai <48792220+sunnytsai3346@users.noreply.github.com>
2024-04-29 12:01:24 -0400

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ProductComponent } from './products/product/product.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    RouterModule,
+    ProductComponent,
+    AddProductComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './products/product/product.component';
+import { AddProductComponent } from './products/add-product/add-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:ProductComponent
+    },
+    {
+        path:'product',
+        component:ProductComponent
+    },
+    {
+        path:'product/add',
+        component:AddProductComponent
+    }
+];
